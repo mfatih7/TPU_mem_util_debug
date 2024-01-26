ce094bd9808bc64ab779925a36d5b4decbb71800
unknown <mfatihaydogdu@gmail.com>
2024-01-26 17:41:14 +0300

    third commit

--- 17_featureMatching/models/models.py
+++ 17_featureMatching/models/models.py
@@ -1,28 +1,16 @@
-from models.models_CNN_Plain import get_model as CNN_Plain_get_model
-from models.models_CNN_Residual import get_model as CNN_Residual_get_model
-from models.models_MobileNetV1 import get_model as MobileNetV1_get_model
-from models.models_MobileNetV2 import get_model as MobileNetV2_get_model
-from models.models_MobileNetV3 import get_model as MobileNetV3_get_model
+from models.models_exp2 import get_model as model_exp2_get_model
 
 import torch
 from torchsummary import summary
 from thop import profile
 from models.torchSummaryWrapper import get_torchSummaryWrapper
         
-def get_model( model_type, N, model_width, bn_or_gn, en_checkpointing ):   
+def get_model( config, model_type, N, model_width, en_checkpointing ):   
     
     if( N == 512 or N == 1024 or N == 2048 or N == 4096 ):        
         
-        if( model_type == 'CNN_Plain' ):    
-            return CNN_Plain_get_model( N, model_width, bn_or_gn, en_checkpointing )    
-        elif( model_type == 'CNN_Residual' ):    
-            return CNN_Residual_get_model( N, model_width, bn_or_gn, en_checkpointing )
-        elif( model_type == 'MobileNetV1' ):
-            return MobileNetV1_get_model( N, bn_or_gn, en_checkpointing )    
-        elif( model_type == 'MobileNetV2' ):
-            return MobileNetV2_get_model( N, bn_or_gn, en_checkpointing )    
-        elif( model_type == 'MobileNetV3' ):
-            return MobileNetV3_get_model( N, bn_or_gn, en_checkpointing )        
+        if( model_type == 'model_exp2' ):
+            return model_exp2_get_model( config, N, model_width, en_checkpointing )
         
         else:
             raise ValueError(f"The provided argument is not valid: {model_type}")
@@ -56,10 +44,23 @@ def get_model( model_type, N, model_width, bn_or_gn, en_checkpointing ):
 def get_model_structure( config, device, model, N, model_width, en_grad_checkpointing):   
     
     if(en_grad_checkpointing==False):
-        summary(model, (config.input_channel_count, N, model_width), batch_size=-1, device=device )
+        summary(model, (config.input_channel_count, N, model_width), batch_size=2, device=device ) # batch_size must be at least 2 to prevent batch norm errors
     else:                    
-        summary(get_torchSummaryWrapper( model ), (config.input_channel_count, N, model_width), batch_size=-1, device=device )
-    input_thop = torch.randn(1, config.input_channel_count, N, model_width, device=device)  # Example input tensor
+        summary(get_torchSummaryWrapper( model ), (config.input_channel_count, N, model_width), batch_size=2, device=device ) # batch_size must be at least 2 to prevent batch norm errors
+    input_thop = torch.randn(2, config.input_channel_count, N, model_width, device=device)  # Example input tensor
     flops, params = profile(model, inputs=(input_thop, ))
-    print(f"Model FLOPs: {flops}")
-    print(f"Model Parameters: {params}")
+    flops = int(flops)
+    params = int(params)
+    print(f"Model FLOPs: {flops:,}")
+    print(f"Model Parameters: {params:,}")
+    
+def get_model_params_and_FLOPS( config, device, model, N, model_width, en_grad_checkpointing):   
+    input_thop = torch.randn(2, config.input_channel_count, N, model_width, device=device)  # Example input tensor
+    flops, params = profile(model, inputs=(input_thop, ))
+    flops = int(flops)
+    params = int(params)
+    print(f"Model FLOPs: {flops:,}")
+    print(f"Model Parameters: {params:,}")
+    
+    return params, flops
+    
